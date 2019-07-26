2751769a6c3fd2ad6cf28e8870be025cb6797e9e
Peng Chen <pchen@thoughtworks.com>
2019-07-26 14:40:45 +0800

    Add Glide as a dependency lib in this gallery loader library

--- galleryloader/src/main/kotlin/cn/visual/galleryloader/GalleryActivity.kt
+++ galleryloader/src/main/kotlin/cn/visual/galleryloader/GalleryActivity.kt
@@ -1,12 +1,18 @@
 package cn.visual.galleryloader
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_gallery.*
 
 class GalleryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+    }
+
+    fun loadFromNetwork(url: String) {
+        Glide.with(this).load(url).into(structure_iv)
     }
 }
